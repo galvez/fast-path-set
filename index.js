0416937fb555cde8889a0747e143bda2d9528aa3
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-24 19:47:49 -0300

    add set()

---
+++ index.js
@@ -0,0 +1,22 @@
+function set (obj, path, value) {
+  const len = path.length
+  let i = 0, prop = '', parent
+  while (i < len) {
+    if (path[i] === '.') {
+      if (prop in obj) {
+        obj = obj[prop]
+        prop = ''
+      } else {
+        parent = obj
+        obj = obj[prop] = {}
+        prop = ''
+      }
+    } else {
+      prop += path[i]
+    }
+    i++
+  }
+  obj[prop] = value
+}
+
+module.exports = set
