d3e3f699e6cc7a90c49a310fc8a00348b721c847
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-25 02:44:27 -0300

    update benchmark script

--- bench.js
+++ bench.js
@@ -14,6 +14,18 @@ if (process.argv.includes('lodash.set')) {
   console.log(e - s)
 }
 
+if (process.argv.includes('dset')) {
+  const { dset: set } = require('dset')
+  const s = performance.now()
+  for (let i = 0; i < 100**4; i++) {
+    const obj = {}
+    set(obj, 'a.b.c.d', 1)
+    set(obj, 'd.c', 2)
+  }
+  const e = performance.now()
+  console.log(e - s)
+}
+
 if (process.argv.includes('fast-path-set')) {
   const set = require('./index')
   const s = performance.now()
