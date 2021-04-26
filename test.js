1f1082f24b802a9db423a72f330b5f5214db17bf
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-26 05:42:20 -0300

    improve on test

--- test.js
+++ test.js
@@ -4,12 +4,23 @@ const tap = require('tap')
 const set = require('./index')
 
 tap.test('should set deeply nested props', (t) => {
-  t.plan(2)
-  const obj = {}
+  t.plan(3)
+  let obj
+  obj = {}
   set(obj, 'a.b.c.d.e.f.g', 1)
   set(obj, 'a.b.x', 2)
   t.ok(obj.a.b.c.d.e.f.g === 1)
   t.ok(obj.a.b.x === 2)
+  obj = {}
+  set(obj, 'foobar.foobar', 1)
+  set(obj, 'a', 1)
+  set(obj, 'something.x.co', 1)
+  set(obj, 'something.xo.x', 1)
+  t.strictSame(obj, {
+    foobar: { foobar: 1 },
+    a: 1,
+    something: { x: { co: 1 }, xo: { x: 1 } }
+  })
 })
 
 tap.test('shouldn\'t try to overwrite non-object values', (t) => {
