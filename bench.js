66a9df22ff4a86d29eabc37be41138a77e2e5f0b
Jonas Galvez <jonasgalvez@gmail.com>
2021-04-26 05:40:37 -0300

    update bench.js with longer keys

--- bench.js
+++ bench.js
@@ -5,35 +5,35 @@ const { performance } = require('perf_hooks')
 if (process.argv.includes('lodash.set')) {
   const set = require('lodash.set')
   const s = performance.now()
-  for (let i = 0; i < 100**4; i++) {
+  for (let i = 0; i < 30 ** 4; i++) {
     const obj = {}
-    set(obj, 'a.b.c.d', 1)
-    set(obj, 'd.c', 2)
+    set(obj, 'aaaa.bbbbb.ccccc.ddddd', 1)
+    set(obj, 'ddddd.ccccc', 2)
   }
   const e = performance.now()
-  console.log(e - s)
+  console.log('lodash.set         ', e - s)
 }
 
 if (process.argv.includes('dset')) {
   const { dset: set } = require('dset')
   const s = performance.now()
-  for (let i = 0; i < 100**4; i++) {
+  for (let i = 0; i < 30 ** 4; i++) {
     const obj = {}
-    set(obj, 'a.b.c.d', 1)
-    set(obj, 'd.c', 2)
+    set(obj, 'aaaa.bbbbb.ccccc.ddddd', 1)
+    set(obj, 'ddddd.ccccc', 2)
   }
   const e = performance.now()
-  console.log(e - s)
+  console.log('dset               ', e - s)
 }
 
 if (process.argv.includes('fast-path-set')) {
   const set = require('./index')
   const s = performance.now()
-  for (let i = 0; i < 100**4; i++) {
+  for (let i = 0; i < 30 ** 4; i++) {
     const obj = {}
-    set(obj, 'a.b.c.d', 1)
-    set(obj, 'd.c', 2)
+    set(obj, 'aaaa.bbbbb.ccccc.ddddd', 1)
+    set(obj, 'ddddd.ccccc', 2)
   }
   const e = performance.now()
-  console.log(e - s)
+  console.log('fast-path-set      ', e - s)
 }
